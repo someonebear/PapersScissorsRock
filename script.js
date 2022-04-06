8a1a13f91c264bd20ff0bab12446a55322368791
Zhihao <zhi.lin1@outlook.com>
2022-04-06 14:29:50 +0800

    Create function to choose random move

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const moveArray = ["Paper", "Scissors", "Rock"];
+
+function randInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let randInt = Math.floor(Math.random() * (max - min + 1) + min);
+  return randInt;
+}
+
 function computerPlay() {
-  console.log("hi")
+  let move = randInt(0, 2);
+  // return moveArray[move];
+  return "hi";
 }
